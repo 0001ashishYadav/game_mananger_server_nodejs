41436ac17960c398cdca38fdd689d1db75af5da5
000ashishYadav <000ashishyadav2003@gmail.com>
2025-09-11 08:33:47 +0530

    home work 2

--- users/router.mjs
+++ users/router.mjs
@@ -1,11 +1,19 @@
 import express from "express";
 const userRouter = express.Router();
-import { forgotPassword, login, resetPassword, signup } from "./controller.mjs";
+import {
+  forgotPassword,
+  getMe,
+  login,
+  resetPassword,
+  signup,
+} from "./controller.mjs";
+import { authentication } from "../auth.mjs";
 
 userRouter
   .post("/signup", signup)
   .post("/login", login)
   .post("/forgot-password", forgotPassword)
-  .post("/reset-password", resetPassword);
+  .post("/reset-password", resetPassword)
+  .get("/profile", authentication, getMe);
 
 export default userRouter;
