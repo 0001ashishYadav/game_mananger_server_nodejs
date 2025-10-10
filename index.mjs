58f217cc52302be27bb99ff791c12162efc066bb
000ashishYadav <000ashishyadav2003@gmail.com>
2025-10-10 15:40:49 +0530

    Update CORS middleware to include PATCH method for API requests

--- index.mjs
+++ index.mjs
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(
   cors({
     origin: "http://localhost:5173", // allow your frontend URL
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true, // if using cookies or auth headers
   })
 );
